ae1d0b84b9e6b1d4e2c8dc7cbb4c4787fe7d2f58
Joshua Lester <jlester3@ewu.edu>
2024-11-02 19:17:49 -0700

    resource list fix2

--- SupportingSpokaneChildren.Web/src/views/Home.vue
+++ SupportingSpokaneChildren.Web/src/views/Home.vue
@@ -3,15 +3,28 @@
     <v-row>
       <v-col cols="12" md="6">
         <AnnouncementsList />
-        <ResourcesList />
+        <ResourcesList v-if="!isMobile" />
       </v-col>
       <v-col cols="12" md="6">
         <EventsList />
+      </v-col>
+      <v-col v-if="isMobile" cols="12" md="6">
+        <ResourcesList />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from 'vuetify/lib/framework.mjs';
+
 useTitle("Home");
+const display = ref(useDisplay());
+
+const isMobile = computed<boolean>(() => {
+  switch (display.value.name) {
+    case 'xs': return true;
+  }
+  return false;
+})
 </script>
